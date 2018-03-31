1bd1443e02accb29b2a0891c2b10f52706fdf544
Rachael Sera <rsera@knights.ucf.edu>
2018-03-30 21:59:24 -0400

    updated squares to be only spiral square

--- turtlesquares.py
+++ turtlesquares.py
@@ -1,25 +1,16 @@
 import turtle
 import random
 
-def square():
-    for i in range(4):
-        turtle.pendown()
-        turtle.forward(50)
-        turtle.right(90)
+length = 5
+numLayers = 100
+colors = ["red", "black", "green", "yellow", "blue", "purple"]
+turtle.speed(0)
 
-def spiralSquare(color):
-    length = 5
-    numLayers = 100
-    colors = ["red", "black", "green", "yellow", "blue", "purple"]
-    turtle.speed(0)
+for i in range(numLayers):
+    turtle.pendown()
+    turtle.forward(length)
+    turtle.right(90)
+    length = length + 5
+    turtle.pencolor(colors[random.randint(0,5)])
 
-    for i in range(numLayers):
-        turtle.pendown()
-        turtle.forward(length)
-        turtle.right(90)
-        length = length + 5
-        if(color == True):
-            turtle.pencolor(colors[random.randint(0,5)])
-
-spiralSquare(True)
     
