cb06abb0decaebf6451d8e2def5e107d8efb73e7
Rachael Sera <rsera@knights.ucf.edu>
2018-10-13 12:17:57 -0400

    in class examples day 1

---
+++ pyOct13.py
@@ -0,0 +1,52 @@
+#print("Hello world")
+#print("Hi")
+# print("Hey")
+#print("Hello\nworld")
+'''
+print("Apples: \t5")
+print("Bananas:\t18")
+print("Oranges:\t1")
+'''
+
+#print(5) # print an int
+#print("5") # print a string
+
+'''
+name = "Rachael" # a string variable
+print(name) # print whats in the variable
+print("name") # prints a string
+
+print("My name is", name)
+'''
+
+'''
+print("Hello " + "World") #concatenation
+print("Hello world " * 3) #repeated concatenation
+'''
+
+#print("Hello", ".", sep="")
+#print("Hello ", end="")
+#print("World")
+
+'''
+name = input("Enter your name\n") #"Rachael"
+print(name)
+'''
+
+'''
+#num = int("6")
+number = int(input("Enter a number\n"))
+print(number)
+
+deci = float(input("Enter a decimal\n"))
+print(deci)
+'''
+
+
+
+
+
+
+
+
+
