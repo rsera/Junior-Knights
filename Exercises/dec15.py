77a7be59b18a4ede3d06b749a18f4b5d4fc5c32d
Rachael Sera <rsera@knights.ucf.edu>
2019-01-25 20:22:22 -0500

    Create dec15.py

---
+++ Exercises/dec15.py
@@ -0,0 +1,73 @@
+
+
+#name = input("enter your name: ")
+
+#print(word)
+#print(name)
+
+#print(length1)
+
+#print(len("dog"))
+'''
+word = "cat"
+
+if 'A' in word:
+    print("Yes")
+else:
+    print("No")
+'''    
+'''
+word = "giraffe"
+i = 0
+length = len(word)
+
+while i < length:
+    print(word[i])
+    i+=1
+'''
+'''
+word = "apple"
+
+for ch in word:
+    print(ch)
+
+'''
+'''
+word = "doggos"
+
+print(word[0:3])
+'''
+
+'''
+animals = ["giraffe", "seal", "uromastyx"]
+print(animals)
+'''
+'''
+animals = []
+
+animals.append("giraffe")
+animals.append("seal")
+animals.append("uromatyx")
+
+for animal in animals:
+    print(animal)
+'''
+
+numbers = [5, 7, 2]
+print(numbers)
+
+numbers.append(3)
+
+print(numbers)
+
+
+
+
+
+
+
+
+
+
+    
+
